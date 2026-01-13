fd56a6459d395d6c1a5976acd0f43d1f31a6534b
abhishek-singh591 <sabhis@qti.qualcomm.com>
2026-01-13 08:46:45 +0000

    Made Minor Fixes
    Signed-off-by: abhishek-singh591 <sabhis@qti.qualcomm.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -2663,7 +2663,10 @@ class QEFFAutoModelForCausalLM(QEFFBaseModel):
             for i in range(self.num_layers):
                 for kv in ["key", "value"]:
                     example_inputs["past_key_values"][i].append(torch.zeros(pkv_cache[0][0].shape, dtype=torch.float32))
-                    dynamic_axes[f"past_{kv}.{i}"] = pkv_dynamic_axes
+                    if kv == "key":
+                        dynamic_axes[f"past_{kv}.{i}"] = pkv_dynamic_axes_key
+                    else:
+                        dynamic_axes[f"past_{kv}.{i}"] = pkv_dynamic_axes_value
                     output_names.append(f"past_{kv}.{i}_RetainedState")
 
         else:
@@ -2707,10 +2710,10 @@ class QEFFAutoModelForCausalLM(QEFFBaseModel):
                         example_inputs["past_key_values"][i].append(
                             torch.zeros(kv_cache_shape[:2] + kv_cache_shape[2:][::-1], dtype=torch.float32)
                         )
-                        dynamic_axes[f"past_key.{i}"] = pkv_dynamic_axes_key[i]
+                        dynamic_axes[f"past_{kv}.{i}"] = pkv_dynamic_axes_key[i]
                     else:
                         example_inputs["past_key_values"][i].append(torch.zeros(kv_cache_shape, dtype=torch.float32))
-                        dynamic_axes[f"past_key.{i}"] = pkv_dynamic_axes_value[i]
+                        dynamic_axes[f"past_{kv}.{i}"] = pkv_dynamic_axes_value[i]
                     output_names.append(f"past_{kv}.{i}_RetainedState")
 
         if self.continuous_batching:
--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from transformers import AutoTokenizer
 from QEfficient import QEFFAutoModelForCausalLM
 
 model_name = "meta-llama/Llama-3.2-1B"
-model1 = QEFFAutoModelForCausalLM.from_pretrained(model_name, num_hidden_layers=4)
-model1.compile(num_devices=1, num_cores=16)
+model1 = QEFFAutoModelForCausalLM.from_pretrained(model_name)
+model1.compile(num_devices=1, num_cores=16, ctx_len=8192)
 hash_0_1 = model1.export_hash
 inputs = "Help me with this"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
